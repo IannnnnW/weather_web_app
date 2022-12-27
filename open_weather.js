8033a2e59fbb9012bd698d56410df318320b036f
@IannnnnW <ianwamai@gmail.com>
2022-12-27 14:14:49 +0300

    resolving the mixed content error

--- open_weather.js
+++ open_weather.js
@@ -7,14 +7,14 @@ class Weather{
 
     //Fetch Location from API
     async  location(){
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&limit=${this.limit}&appid=${this.apiKey}`);
+        const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${this.cityName}&limit=${this.limit}&appid=${this.apiKey}`);
         const latLong = await response.json();
         return latLong;
     }
 
     //Fect weather from API
     async  weatherCondition(lat, lon){
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`);
         const condition = await response.json();
         return condition;
     }
